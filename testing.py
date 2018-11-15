c0b0f243f260509f9a5a5d00bc69a46b1bab0347
bharad96 <bharadwaj6991@gmail.com>
2018-11-15 22:14:09 +0800

    updated a spelling mistake

--- testing.py
+++ testing.py
@@ -5,22 +5,6 @@ import web_scrapper
 
 pygame.init()
 
-#should return a string url
-#def convert_coordiantes_walking_html(from_coordinates, destination):
-#    # url for reference 
-#    # http://maps.ntu.edu.sg/m?q=1.3535285933806145%2C+103.68827099377593&sch_btn=Go&font=+m&t=+Pioneer+Food+Court
-#    return 'http://maps.ntu.edu.sg/m?q=' + str(from_coordinates[0]) + '%2C+' + str(from_coordinates[1]) + '&sch_btn=Go&font=+m&t=+' + '+'.join(destination.split(' '))
-#
-#def convert_coordinates_bus_html(from_coordinates, destination):
-#    # usually for bus there are three options
-#    # lst of the three bus options
-#    bus_lst = []
-#    # http://maps.ntu.edu.sg/m?q=Tanjong%20Hall%20of%20Residence%20-%20Block%2020B%20to%20Pioneer%20Food%20Court&d=b&p=0&fs=m
-#    bus_0 = 'http://maps.ntu.edu.sg/m?q=' + str(from_coordinates[0]) + '%2C+' + str(from_coordinates[1]) + '%20to%20' + '+'.join(destination.split(' ')) + '&d=b&p=0&fs=m'
-#    bus_1 = 'http://maps.ntu.edu.sg/m?q=' + str(from_coordinates[0]) + '%2C+' + str(from_coordinates[1]) + '%20to%20' + '+'.join(destination.split(' ')) + '&d=b&p=1&fs=m'
-#    bus_2 = 'http://maps.ntu.edu.sg/m?q=' + str(from_coordinates[0]) + '%2C+' + str(from_coordinates[1]) + '%20to%20' + '+'.join(destination.split(' ')) + '&d=b&p=2&fs=m'
-#    bus_lst.extend([bus_0, bus_1, bus_2]) 
-#    return bus_lst
 
 # take this to be top left point
 hall_12_bus_stop_coordinates = (1.351937, 103.680543)
@@ -55,8 +39,8 @@ def event_handler():
             #print(difference_in_pixels)
             ratio = ((click[0]-hall_12_pixel_coordinates[0])/difference_in_pixels[0], (click[1]-hall_12_pixel_coordinates[1])/difference_in_pixels[1])
             #print(ratio)
-            new_cooridnates = (hall_12_bus_stop_coordinates[0]+difference_in_coordinates[0]*ratio[1], hall_12_bus_stop_coordinates[1]+difference_in_coordinates[1]*ratio[0])
-            print(new_cooridnates)
+            new_coordinates = (hall_12_bus_stop_coordinates[0]+difference_in_coordinates[0]*ratio[1], hall_12_bus_stop_coordinates[1]+difference_in_coordinates[1]*ratio[0])
+            print(new_coordinates)
             return new_coordinates
         
 #            destination = "Pioneer Food Court"
