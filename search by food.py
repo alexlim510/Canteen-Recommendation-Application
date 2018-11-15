901c172d1b4a28a9252705a629c1f0aaef39feff
alexlim56 <42902719+alexlim56@users.noreply.github.com>
2018-11-15 17:07:39 +0800

    Update search by food.py

--- search by food.py
+++ search by food.py
@@ -13,27 +13,12 @@ def get_canteen_booth(canteendata,user_food):
                     canteen_booth_list.append((canteen,booths))
     return canteen_booth_list
 
-#multiple booths in canteen
-def print_canteen_booth(canteen_booth_list):        
-    canteen_list = []
-    booth_list = []
-
-    boothlist = "list of booths: "
-    for item in canteen_booth_list:
-        boothlist = boothlist + ',' + item[1]
-
-    canteenlist = "list of canteens: "
-    for item in canteen_booth_list:
-        canteenlist = canteenlist + ',' + item[0]
-
-    
 
 def printbooth(canteen_booth_list, root):
     for item in canteen_booth_list:
         Label(root, text = item[0] + ': ' + item[1]).pack()
 
         
-
 def search_by_food(canteendata, user_food, root):
     canteen_booth_list = get_canteen_booth(canteendata, user_food)
     if canteen_booth_list == []:
