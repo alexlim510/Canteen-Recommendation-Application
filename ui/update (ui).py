b63ef6840ddab0fb8018f646c86edb66c05a44bc
alexlim56 <42902719+alexlim56@users.noreply.github.com>
2018-11-16 18:38:05 +0800

    Update update (ui).py

--- ui/update (ui).py
+++ ui/update (ui).py
@@ -1,14 +1,12 @@
 import json
 from tkinter import *
 
-
 with open('canteen.json', 'r') as f:
     canteendata = json.load(f)
 #Split into 2 major parts
 # 1) Add or Change data
 # 2) Remove data
 ###################################################################################################################################################
-
 #functions to check if data exists
 
 
@@ -62,11 +60,12 @@ def add_canteen(canteendata):
     Label(ac_screen, text = "Please input canteen to add:").grid(row=0)
     canteen = Entry(ac_screen)
     canteen.grid(row=0,column=1)
-    Button(ac_screen, text="submit", command=lambda:obtain_canteen_ac(canteendata, canteen)).grid(row=0,column=2)
+    Button(ac_screen, text="submit", command=lambda:obtain_canteen_ac(canteendata, canteen,ac_screen)).grid(row=0,column=2)
 
 
-def obtain_canteen_ac(canteendata, canteen):
+def obtain_canteen_ac(canteendata, canteen, ac_screen):
     canteen = canteen.get()
+    ac_screen.destroy()
     ac_f2 = Toplevel()
     msg_ac = StringVar()
     Label(ac_f2, textvariable = msg_ac).pack()
@@ -91,10 +90,11 @@ def add_booth(canteendata):
     Label(ab_screen, text = "Please input canteen with booth:").grid(row=0)
     canteen = Entry(ab_screen)
     canteen.grid(row=0,column=1)
-    Button(ab_screen, text="submit", command=lambda:obtain_canteen_ab(canteendata, canteen)).grid(row=0,column=2)
+    Button(ab_screen, text="submit", command=lambda:obtain_canteen_ab(canteendata, canteen,ab_screen)).grid(row=0,column=2)
 
-def obtain_canteen_ab(canteendata, canteen):
+def obtain_canteen_ab(canteendata, canteen, ab_screen):
     canteen = canteen.get()
+    ab_screen.destroy()
     ab_f2 = Toplevel()
     msg_ab1 = StringVar()
     Label(ab_f2, textvariable = msg_ab1).grid(row=1)
@@ -102,13 +102,14 @@ def obtain_canteen_ab(canteendata, canteen):
         Label(ab_f2, text = "Please input booth to add:").grid(row=0)
         booth = Entry(ab_f2)
         booth.grid(row=0,column=1)
-        Button(ab_f2, text="submit", command=lambda:obtain_booth_ab(canteendata,canteen, booth)).grid(row=0,column=2)
+        Button(ab_f2, text="submit", command=lambda:obtain_booth_ab(canteendata,canteen, booth,ab_f2)).grid(row=0,column=2)
     else:
         msg_ab1.set("Please input a valid canteen.")
 
 #retrieve and check booth
-def obtain_booth_ab(canteendata, canteen, booth):
+def obtain_booth_ab(canteendata, canteen, booth,ab_f2):
     booth = booth.get()
+    ab_f2.destroy()
     ab_f3 = Toplevel()
     msg_ab2 = StringVar()
     Label(ab_f3, textvariable = msg_ab2).pack()
@@ -130,11 +131,12 @@ def add_food(canteendata):
     Label(af_screen, text = "Please input canteen with food:").grid(row=0)
     canteen = Entry(af_screen)
     canteen.grid(row=0,column=1)
-    Button(af_screen, text="submit", command=lambda:obtain_canteen_af(canteendata,canteen)).grid(row=0,column=2)
+    Button(af_screen, text="submit", command=lambda:obtain_canteen_af(canteendata,canteen,af_screen)).grid(row=0,column=2)
 
 
-def obtain_canteen_af(canteendata, canteen):
+def obtain_canteen_af(canteendata, canteen, af_screen):
     canteen = canteen.get()
+    af_screen.destroy()
     af_f2 = Toplevel()
     msg_af1 = StringVar()
     Label(af_f2, textvariable = msg_af1).grid(row=1)
@@ -143,14 +145,15 @@ def obtain_canteen_af(canteendata, canteen):
             Label(af_f2, text = "Please input booth with food:").grid(row=0)
             booth = Entry(af_f2)
             booth.grid(row=0,column=1)
-            Button(af_f2, text="submit", command=lambda:obtain_booth_af(canteendata,canteen, booth)).grid(row=0,column=2)
+            Button(af_f2, text="submit", command=lambda:obtain_booth_af(canteendata,canteen, booth,af_f2)).grid(row=0,column=2)
         else:
             msg_af1.set("This canteen does not have booth.")
     else:
         msg_af1.set("Please input a valid canteen.")
 
-def obtain_booth_af(canteendata, canteen, booth):
+def obtain_booth_af(canteendata, canteen, booth, af_f2):
     booth = booth.get()
+    af_f2.destroy()
     af_f3 = Toplevel()
     msg_af3 = StringVar()
     Label(af_f3, textvariable = msg_af3).grid(row=1)
@@ -158,12 +161,13 @@ def obtain_booth_af(canteendata, canteen, booth):
         Label(af_f3, text = "Please input food to add:").grid(row=0)
         food = Entry(af_f3)
         food.grid(row=0,column=1)
-        Button(af_f3, text="submit", command=lambda:obtain_food_af(canteendata,canteen,booth,food)).grid(row=0,column=2)
+        Button(af_f3, text="submit", command=lambda:obtain_food_af(canteendata,canteen,booth,food,af_f3)).grid(row=0,column=2)
     else:
         msg_af3.set("Please input a valid booth.")
 
-def obtain_food_af(canteendata, canteen, booth,food):
+def obtain_food_af(canteendata, canteen, booth,food,af_f3):
     food = food.get()
+    af_f3.destroy()
     af_f4 = Toplevel()
     msg_af4 = StringVar()
     Label(af_f4, textvariable = msg_af4).grid(row=1)
@@ -173,11 +177,12 @@ def obtain_food_af(canteendata, canteen, booth,food):
         Label(af_f4, text = "Please input price:").grid(row=0)
         price = Entry(af_f4)
         price.grid(row=0,column=1)
-        Button(af_f4, text="submit", command=lambda:obtain_price_af(canteendata,canteen,booth,food,price)).grid(row=0,column=2)
+        Button(af_f4, text="submit", command=lambda:obtain_price_af(canteendata,canteen,booth,food,price,af_f4)).grid(row=0,column=2)
 
 
-def obtain_price_af(canteendata, canteen, booth, food, price):
+def obtain_price_af(canteendata, canteen, booth, food, price, af_f4):
     price = price.get()
+    af_f4.destroy()
     af_f5 = Toplevel()
     msg_af5 = StringVar()
     Label(af_f5, textvariable = msg_af5).pack()
@@ -206,10 +211,11 @@ def change_price(canteendata):
     Label(cp_screen, text = "Please input canteen with food:").grid(row=0)
     canteen = Entry(cp_screen)
     canteen.grid(row=0,column=1)
-    Button(cp_screen, text="submit", command=lambda:obtain_canteen_pc(canteendata,canteen)).grid(row=0,column=2)
+    Button(cp_screen, text="submit", command=lambda:obtain_canteen_pc(canteendata,canteen,cp_screen)).grid(row=0,column=2)
 
-def obtain_canteen_pc(canteendata, canteen):
+def obtain_canteen_pc(canteendata, canteen, cp_screen):
     canteen = canteen.get()
+    cp_screen.destroy()
     cp_f2 = Toplevel()
     msg_cp1 = StringVar()
     Label(cp_f2, textvariable = msg_cp1).grid(row=1)
@@ -218,14 +224,15 @@ def obtain_canteen_pc(canteendata, canteen):
             Label(cp_f2, text = "Please input booth with food:").grid(row=0)
             booth = Entry(cp_f2)
             booth.grid(row=0,column=1)
-            Button(cp_f2, text="submit", command=lambda:obtain_booth_pc(canteendata,canteen, booth)).grid(row=0,column=2)
+            Button(cp_f2, text="submit", command=lambda:obtain_booth_pc(canteendata,canteen, booth, cp_f2)).grid(row=0,column=2)
         else:
             msg_cp1.set("This canteen does not have booth.")
     else:
         msg_cp1.set("Please input a valid canteen.")
 
-def obtain_booth_pc(canteendata, canteen, booth):
+def obtain_booth_pc(canteendata, canteen, booth, cp_f2):
     booth = booth.get()
+    cp_f2.destroy()
     cp_f3 = Toplevel()
     msg_cp3 = StringVar()
     Label(cp_f3, textvariable = msg_cp3).grid(row=1)
@@ -234,14 +241,15 @@ def obtain_booth_pc(canteendata, canteen, booth):
             Label(cp_f3, text = "Please input food:").grid(row=0)
             food = Entry(cp_f3)
             food.grid(row=0,column=1)
-            Button(cp_f3, text="submit", command=lambda:obtain_food_pc(canteendata,canteen,booth,food)).grid(row=0,column=2)
+            Button(cp_f3, text="submit", command=lambda:obtain_food_pc(canteendata,canteen,booth,food,cp_f3)).grid(row=0,column=2)
         else:
             msg_cp3.set("This booth does not have food.")
     else:
         msg_cp3.set("Please input a valid booth.")
 
-def obtain_food_pc(canteendata, canteen, booth,food):
+def obtain_food_pc(canteendata, canteen, booth,food,cp_f3):
     food = food.get()
+    cp_f3.destroy()
     cp_f4 = Toplevel()
     msg_cp4 = StringVar()
     Label(cp_f4, textvariable = msg_cp4).grid(row=1)
@@ -249,13 +257,14 @@ def obtain_food_pc(canteendata, canteen, booth,food):
         Label(cp_f4, text = "Please input new price:").grid(row=0)
         price = Entry(cp_f4)
         price.grid(row=0,column=1)
-        Button(cp_f4, text="submit", command=lambda:obtain_price_pc(canteendata,canteen,booth,food,price)).grid(row=0,column=2)
+        Button(cp_f4, text="submit", command=lambda:obtain_price_pc(canteendata,canteen,booth,food,price,cp_f4)).grid(row=0,column=2)
     else:
         msg_cp4.set("This food does not exist.")
 
 
-def obtain_price_pc(canteendata, canteen, booth, food, price):
+def obtain_price_pc(canteendata, canteen, booth, food, price,cp_f4):
     price = price.get()
+    cp_f4.destroy()
     cp_f5 = Toplevel()
     msg_cp5 = StringVar()
     Label(cp_f5, textvariable = msg_cp5).pack()
@@ -285,10 +294,11 @@ def add_change_address(canteendata):
     Label(aa_screen, text = "Please input canteen:").grid(row=0)
     canteen = Entry(aa_screen)
     canteen.grid(row=0,column=1)
-    Button(aa_screen, text="submit", command=lambda:obtain_address_aa(canteendata,canteen)).grid(row=0,column=2)
+    Button(aa_screen, text="submit", command=lambda:obtain_canteen_aa(canteendata,canteen,aa_screen)).grid(row=0,column=2)
 
-def obtain_address_aa(canteendata, canteen):
+def obtain_canteen_aa(canteendata, canteen,aa_screen):
     canteen = canteen.get()
+    aa_screen.destroy()
     aa_f2 = Toplevel()
     msg_aa1 = StringVar()
     Label(aa_f2, textvariable = msg_aa1).grid(row=1)
@@ -296,13 +306,14 @@ def obtain_address_aa(canteendata, canteen):
             Label(aa_f2, text = "Please input address:").grid(row=0)
             address = Entry(aa_f2)
             address.grid(row=0,column=1)
-            Button(aa_f2, text="submit", command=lambda:obtain_address_aa(canteendata,canteen, address)).grid(row=0,column=2)
+            Button(aa_f2, text="submit", command=lambda:obtain_address_aa(canteendata,canteen, address,aa_f2)).grid(row=0,column=2)
     else:
         msg_aa1.set("Please input a valid canteen.")
 
 
-def obtain_address_aa(canteendata, canteen, address):
+def obtain_address_aa(canteendata, canteen, address,aa_f2):
     address = address.get()
+    aa_f2.destroy()
     aa_f3 = Toplevel()
     Label(aa_f3, text= "Done").pack()
     change_address(canteendata, canteen, address)
@@ -319,10 +330,11 @@ def add_change_rank(canteendata):
     Label(ar_screen, text = "Please input canteen:").grid(row=0)
     canteen = Entry(ar_screen)
     canteen.grid(row=0,column=1)
-    Button(ar_screen, text="submit", command=lambda:obtain_rank(canteendata,canteen)).grid(row=0,column=2)
+    Button(ar_screen, text="submit", command=lambda:obtain_rank(canteendata,canteen,ar_screen)).grid(row=0,column=2)
 
-def obtain_rank(canteendata, canteen):
+def obtain_rank(canteendata, canteen, ar_screen):
     canteen = canteen.get()
+    ar_screen.destroy()
     ar_f2 = Toplevel()
     msg_ar1 = StringVar()
     Label(ar_f2, textvariable = msg_ar1).grid(row=1)
@@ -330,13 +342,14 @@ def obtain_rank(canteendata, canteen):
             Label(ar_f2, text = "Please input rank:").grid(row=0)
             rank = Entry(ar_f2)
             rank.grid(row=0,column=1)
-            Button(ar_f2, text="submit", command=lambda:obtain_rank_ar(canteendata,canteen, rank)).grid(row=0,column=2)
+            Button(ar_f2, text="submit", command=lambda:obtain_rank_ar(canteendata,canteen, rank,ar_f2)).grid(row=0,column=2)
     else:
         msg_ar1.set("Please input a valid canteen.")
 
 
-def obtain_rank_ar(canteendata, canteen, rank):
+def obtain_rank_ar(canteendata, canteen, rank,ar_f2):
     rank = rank.get()
+    ar_f2.destroy()
     ar_f3 = Toplevel()
     Label(ar_f3, text= "Done").pack()
     change_rank(canteendata, canteen, rank)
@@ -366,12 +379,13 @@ def remove_canteen():
     Label(rc_screen, text = "Please input canteen to remove:").grid(row=0)
     canteen = Entry(rc_screen)
     canteen.grid(row=0,column=1)
-    Button(rc_screen, text="submit", command=lambda:obtain_canteen_cr(canteendata, canteen)).grid(row=0,column=2)
+    Button(rc_screen, text="submit", command=lambda:obtain_canteen_cr(canteendata, canteen,rc_screen)).grid(row=0,column=2)
 
 
 #obtain canteen to remove
-def obtain_canteen_cr(canteendata, canteen):
+def obtain_canteen_cr(canteendata, canteen,rc_screen):
     canteen = canteen.get()
+    rc_screen.destroy()
     rc_f2 = Toplevel()
     msg_rc = StringVar()
     Label(rc_f2, textvariable = msg_rc).pack()
@@ -395,28 +409,30 @@ def remove_booth():
     Label(rb_screen, text = "Please input canteen with booth:").grid(row=0)
     canteen = Entry(rb_screen)
     canteen.grid(row=0,column=1)
-    Button(rb_screen, text="submit", command=lambda:obtain_canteen_br(canteendata,canteen)).grid(row=0,column=2)
+    Button(rb_screen, text="submit", command=lambda:obtain_canteen_br(canteendata,canteen,rb_screen)).grid(row=0,column=2)
 
 
-def obtain_canteen_br(canteendata, canteen):
+def obtain_canteen_br(canteendata, canteen, rb_screen):
     canteen = canteen.get()
+    rb_screen.destroy()
     rb_f2 = Toplevel()
     msg_rb1 = StringVar()
     Label(rb_f2, textvariable = msg_rb1).grid(row=1)
     if check_canteen(canteendata, canteen):
-        if any(canteendata['canteen'][canteen]['booths'].values()):
+        if any(canteendata['canteen'][canteen]['booths'].items()):
             Label(rb_f2, text = "Please input booth to remove:").grid(row=0)
             booth = Entry(rb_f2)
             booth.grid(row=0,column=1)
-            Button(rb_f2, text="submit", command=lambda:obtain_booth_br(canteendata,canteen,booth)).grid(row=0,column=2)
+            Button(rb_f2, text="submit", command=lambda:obtain_booth_br(canteendata,canteen,booth,rb_f2)).grid(row=0,column=2)
         else:
             msg_rb1.set("This canteen does not have booth.")
     else:
         msg_rb1.set("Please input a valid canteen.")
 
 
-def obtain_booth_br(canteendata, canteen, booth):
+def obtain_booth_br(canteendata, canteen, booth,rb_f2):
     booth = booth.get()
+    rb_f2.destroy()
     rb_f3 = Toplevel()
     msg_rb2 = StringVar()
     Label(rb_f3, textvariable = msg_rb2).pack()
@@ -438,37 +454,39 @@ def remove_food():
     Label(rf_screen, text = "Please input canteen with food:").grid(row=0)
     canteen = Entry(rf_screen)
     canteen.grid(row=0,column=1)
-    Button(rf_screen, text="submit", command=lambda:obtain_canteen_fr(canteendata,canteen)).grid(row=0,column=2)
+    Button(rf_screen, text="submit", command=lambda:obtain_canteen_fr(canteendata,canteen,rf_screen)).grid(row=0,column=2)
 
 
-def obtain_canteen_fr(canteendata, canteen):
+def obtain_canteen_fr(canteendata, canteen,rf_screen):
     canteen = canteen.get()
+    rf_screen.destroy()
     rf_f2 = Toplevel()
     msg_rf1 = StringVar()
     Label(rf_f2, textvariable = msg_rf1).grid(row=1)
     if check_canteen(canteendata, canteen):
-        if any(canteendata['canteen'][canteen]['booths'].values()):
+        if any(canteendata['canteen'][canteen]['booths'].items()):
             Label(rf_f2, text = "Please input booth with food:").grid(row=0)
             booth = Entry(rf_f2)
             booth.grid(row=0,column=1)
-            Button(rf_f2, text="submit", command=lambda:obtain_booth_fr(canteendata,canteen, booth)).grid(row=0,column=2)
+            Button(rf_f2, text="submit", command=lambda:obtain_booth_fr(canteendata,canteen, booth,rf_f2)).grid(row=0,column=2)
         else:
             msg_rf1.set("This canteen does not have booth.")
     else:
         msg_rf1.set("Please input a valid canteen.")
 
 
-def obtain_booth_fr(canteendata, canteen, booth):
+def obtain_booth_fr(canteendata, canteen, booth,rf_f2):
     booth = booth.get()
+    rf_f2.destroy()
     rf_f3 = Toplevel()
     msg_rf3 = StringVar()
     Label(rf_f3, textvariable = msg_rf3).grid(row=1)
     if check_booth(canteendata, canteen, booth):
-        if any(canteendata['canteen'][canteen]['booths'][booth].values()):
+        if any(canteendata['canteen'][canteen]['booths'][booth].items()):
             Label(rf_f3, text = "Please input food to remove:").grid(row=0)
             food = Entry(rf_f3)
             food.grid(row=0,column=1)
-            Button(rf_f3, text="submit", command=lambda:obtain_food_fr(canteendata,canteen,booth,food)).grid(row=0,column=2)
+            Button(rf_f3, text="submit", command=lambda:obtain_food_fr(canteendata,canteen,booth,food,rf_f3)).grid(row=0,column=2)
         else:
             msg_rf3.set("This booth does not have food.")
     else:
@@ -476,8 +494,9 @@ def obtain_booth_fr(canteendata, canteen, booth):
 
             
 
-def obtain_food_fr(canteendata, canteen, booth, food):
+def obtain_food_fr(canteendata, canteen, booth, food,rf_f3):
     food = food.get()
+    rf_f3.destroy()
     rf_f4 = Toplevel()
     msg_rf3 = StringVar()
     Label(rf_f4, textvariable = msg_rf3).pack()
